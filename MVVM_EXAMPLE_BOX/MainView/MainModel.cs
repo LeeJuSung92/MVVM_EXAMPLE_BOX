e7bccb55e1bd38bdaba0d0de14a7dd417dfd1e5b
LeeJuSung92 <sadfly123@gmail.com>
2022-04-26 20:07:05 +0900

    Text_Move_Text
    텍스트 박스가 같이 변하는 예제를 추가합니다.

---
+++ MVVM_EXAMPLE_BOX/MainView/MainModel.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel;
+
+namespace MVVM_EXAMPLE_BOX.MainView
+{
+    internal class MainModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private string _Str1;
+
+        public string str1
+        {
+            get => _Str1;
+            set
+            {
+                _Str1 = value;
+                _Str2 = _Str1;
+                OnPropertyChanged("STR2");
+            }
+
+        }
+
+        private string _Str2;
+
+        public string str2
+        {
+            get => _Str2;
+            set
+            {
+                _Str2 = value;
+                OnPropertyChanged("STR1");
+            }
+        }
+
+
+
+
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+    }
+}
+
---
+++ MVVM_EXAMPLE_BOX/MainView/MainViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+
+namespace MVVM_EXAMPLE_BOX.MainView
+{
+    internal class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private MainView.MainModel model;
+
+        public MainViewModel()
+        {
+            model = new MainView.MainModel();
+        }
+
+        public MainView.MainModel Model
+        {
+            get => model;
+            set { model = value; OnPropertyChanged("Model"); }
+        }
+
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+
+    }
+}
